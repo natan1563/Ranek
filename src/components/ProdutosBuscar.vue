d2fac6898e00ea524232ed17ced7ead632035b65
natan1563 <natromafe@gmail.com>
2021-09-29 21:50:05 -0300

    Estilos e helper serialize

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,11 +1,22 @@
 <template>
-  <!-- <input type="text"> -->
-  <span></span>
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca">
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+  </form>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      busca: ""
+    }
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({query: {q: this.busca}});
+    }
+  },
 }
 </script>
 
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,31 +1,50 @@
 <template>
   <section class="produtos-container">
-   <div v-for="produto in produtos" :key="produto.id">
-     <!-- <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo"> -->
-     <img v-if="produto.fotos">
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="descricao">{{ produto.descricao }}</p>
+   <div v-if="produtos && produtos.length" class="produtos">
+     <div v-for="produto in produtos" :key="produto.id" class="produto">
+       <router-link to="/">
+          <!-- <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo"> -->
+          <img v-if="produto.fotos">
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p class="descricao">{{ produto.descricao }}</p>
+       </router-link>
+    </div>
+   </div>
+   <div v-else-if="produtos && produtos.length === 0">
+     <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
    </div>
   </section>
 </template>
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from '@/helpers.js';
 
 export default {
   data() {
     return {
-      produtos: null
+      produtos: null,
+      produtosPorPagina: 9
+    }
+  },
+  computed: {
+    url() {
+      return `/produto?_limit=${this.produtosPorPagina}${serialize(this.$route.query)}`;
     }
   },
   methods: {
     getProdutos(){
-      api.get('/produto')
+      api.get(this.url)
         .then(response => {
           console.log(response);
           this.produtos = response.data;
         });
+    }
+  },
+  watch: {
+    url() {
+      this.getProdutos();
     }
   },
   created() {
@@ -34,6 +53,50 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+  .produtos-container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+  .produtos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin: 30px;
+  }
+
+  .produto {
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+    padding: 6px;
+    background: #fff;
+    border-radius: 4px;
+    transition: all 0.2s;
+  }
+
+  .produto:hover {
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+    transform: scale(1.1);
+    position: relative;
+    z-index: 1;
+  }
+
+  .produto img {
+    border-radius: 4px;
+    margin-bottom: 20px;
+  }
+
+  .titulo {
+    margin-bottom: 10px;
+  }
+
+  .preco {
+    font-weight: bold;
+    color: #e80;
+  }
+
+  .sem-resultados {
+    text-align: center;
+  }
 </style>
